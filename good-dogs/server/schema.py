3cd2c8ce0d15eeab7ebe4888d69ff8f6617cd016
Tobias Patton <tobias.patton@semaphoresolutions.ca>
2020-06-26 14:52:48 -0700

    Fix spelling error

--- good-dogs/server/schema.py
+++ good-dogs/server/schema.py
@@ -7,5 +7,5 @@ class BreedSummary(Schema):
 class Breed(BreedSummary):
     weight_pounds = fields.List(fields.Float())
     height_inches = fields.List(fields.Float())
-    life_expectency = fields.List(fields.Integer())
+    life_expectancy = fields.List(fields.Integer())
     other_names = fields.List(fields.Str())
