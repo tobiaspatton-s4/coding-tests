61687233ff0eba223ca0392f5a144aa0278f917d
Tobias Patton <tobias.patton@semaphoresolutions.ca>
2020-06-26 16:00:17 -0700

    Adding bootstrap styling

--- good-dogs/ui/src/app/app.module.ts
+++ good-dogs/ui/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DogListComponent } from './dog-list/dog-list.component';
 import { DogDetailsComponent } from './dog-details/dog-details.component';
+import { DogListComponent } from './dog-list/dog-list.component';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { DogDetailsComponent } from './dog-details/dog-details.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
